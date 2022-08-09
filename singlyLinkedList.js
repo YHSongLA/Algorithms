609e338ff19ed94375ef95ec0434fb72b56b0b23
Yo Han Song <jsong061@gmail.com>
2022-08-09 13:16:07 -0700

    Added InsertAtBack and InsertAtBackRecursive to singlyLinkedList

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -20,12 +20,13 @@ class SinglyLinkedList {
     printValues() {
         // if the list it is empty
         if (this.head === null) {
-            console.log("LIST IS EMPTY")
-            return
+            // console.log("LIST IS EMPTY")
+            return this
         }
         // if the list is not empty
         var runner = this.head
-        // check to see if runner.next is not equal to null
+        // check to see if runner.next is not equal to null, 
+        // but have the runner stop at the last node because .next will point to null
         while (runner.next != null) {
             console.log(runner.value)
             // to move the runner
@@ -38,13 +39,13 @@ class SinglyLinkedList {
     contains(target) {
         // check to see if the list empty
         if (this.head === null) {
-            console.log("LIST IS EMPTY")
-            return
+            // console.log("LIST IS EMPTY")
+            return this
         }
         // create a runner variable and assign it to this.head
         var runner = this.head
-        // check to see if the runner is not null
-        while (runner.next != null) {
+        // check while runner is not equals to null, but have the runner run out of bounds of the linked list
+        while (runner != null) {
             // if target equals to runner.value return true
             if (target === runner.value) {
                 return true
@@ -54,13 +55,59 @@ class SinglyLinkedList {
                 runner = runner.next
             }
         }
-        // check to see if the target equals to the value, if so return true
-        if (target === runner.value) {
-            return true
+        return false
+    }
+
+    insertAtFront(node) {
+        const newNode = new SLNode(node)
+        newNode.next = this.head
+        this.head = newNode
+        return this
+    }
+
+    /**
+     * Creates a new node with the given data and inserts it at the back of
+     * this list.
+     * - Time: (?).
+     * - Space: (?).
+     * @param {any} data The data to be added to the new node.
+     * @returns {SinglyLinkedList} This list.
+     */
+     insertAtBack(data) {
+        const newBackNode = new SLNode(data);
+        let runner = this.head;
+
+        if (runner === null) {
+            this.head = newBackNode;
+        } else {
+            while (runner.next) {
+                runner = runner.next;
+            }
+            runner.next = newBackNode;
         }
-        // otherwise return false if not the target is not in the list
-        else {
-            return false
+        return this;
+    }
+
+    insertAtBackRecursive(data, runner = this.head) {
+        if (runner.next !== true) {
+            runner.next = new SLNode(data)
+            return this
+        } else {
+            return this.insertAtBackRecursive(data, runner.next)
         }
     }
 }
+
+var sll = new SinglyLinkedList();
+sll.insertAtBack(1)
+sll.insertAtBack(2)
+sll.insertAtBack(3)
+sll.insertAtBack(4)
+sll.insertAtBack(5)
+sll.printValues()
+// console.log(sll.insertAtBack().printValues())
+
+// sll.insertAtFront(10).insertAtFront(10).insertAtFront(10)
+// // sll.removeAtFront()
+// sll.printValues()
+// console.log(sll.insertAtFront())
