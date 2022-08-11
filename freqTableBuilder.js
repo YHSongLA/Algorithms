c4777f2617afec5d7ec095f4b7ca5028c3cf81c8
Yo Han Song <jsong061@gmail.com>
2022-08-11 15:29:27 -0700

    Frequency Table Builder algorithm added

---
+++ freqTableBuilder.js
@@ -0,0 +1,36 @@
+const freqTableBuilder = require("./freqTable");
+// // https://app.codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/
+
+
+/* 
+Given a non-empty array of odd length containing ints where every int but one
+has a matching pair (another int that is the same)
+return the only int that has no matching pair.
+*/
+
+const nums1 = [1];
+const expected1 = 1;
+
+const nums2 = [5, 4, 5];
+const expected2 = 4;
+
+const nums3 = [5, 4, 3, 4, 3, 4, 5];
+const expected3 = 4; // there is a pair of 4s but one 4 has no pair.
+
+const nums4 = [5, 2, 6, 2, 3, 1, 6, 3, 2, 5, 2];
+const expected4 = 1;
+
+function oddOccurrencesInArray(nums) {
+    const freqTable = freqTableBuilder(nums)
+
+    for (let i in freqTable) {
+        if (freqTable[i] % 2 !== 0) {
+            return i
+        }
+    }
+}
+
+console.log(oddOccurrencesInArray(nums1))
+console.log(oddOccurrencesInArray(nums2))
+console.log(oddOccurrencesInArray(nums3))
+console.log(oddOccurrencesInArray(nums4))
