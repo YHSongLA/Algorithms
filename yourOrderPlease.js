db94c3cba9c4a18c5dfa63d61225dd8b621a036d
Yo Han Song <jsong061@gmail.com>
2022-08-19 12:53:42 -0700

    Your order please algorithm added nested for loop to sort string

---
+++ yourOrderPlease.js
@@ -0,0 +1,38 @@
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+// Examples
+
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+function order(words) {
+    if (words.length === 0) {
+        return ""
+    }
+
+    let myWords = words.split(" ")
+    let sorted = []
+
+    for (let i = 0; i <= myWords.length; i++) {
+        for (let j = 0; j < myWords.length; j++) {
+            if (myWords[j].indexOf(i) >= 0) {
+                sorted.push(myWords[j])
+            }
+        }
+    }
+    return sorted.join(" ")
+}
+
+const {assert} = require('chai');
+
+describe("order", () => {
+  it("should work corectly", () => {
+    assert.strictEqual(order("is2 Thi1s T4est 3a"), "Thi1s is2 3a T4est")
+    assert.strictEqual(order("4of Fo1r pe6ople g3ood th5e the2"), "Fo1r the2 g3ood 4of th5e pe6ople")
+    assert.strictEqual(order(""), "", "empty input should return empty string" )
+  });
+});
