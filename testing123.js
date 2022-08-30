b49ce04f3315908e847c05d12959ddd121c0c111
Yo Han Song <jsong061@gmail.com>
2022-08-30 12:35:32 -0700

    Testing 1-2-3 algorithm added

---
+++ testing123.js
@@ -0,0 +1,35 @@
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+
+var number = function (array) {
+    let myArr = []
+
+    if (array.length === 0) {
+        return myArr
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        myArr.push((i + 1) + ": " + array[i])
+    }
+    return myArr
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.deepEqual(number([]), [], 'Empty array should return empty array');
+    assert.deepEqual(number(["a", "b", "c"]), ["1: a", "2: b", "3: c"], 'Return the correct line numbers');                
+  });
+});
