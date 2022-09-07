9ba545e70b7e55dbb19e4aea0f50e6e8529c5e7a
Yo Han Song <jsong061@gmail.com>
2022-09-07 15:45:55 -0700

    Reverse word algorithm added

---
+++ reverseWords.js
@@ -0,0 +1,34 @@
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+// Examples
+
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+function reverseWords(str) {
+    let revWord = ''
+    let revStr = ''
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== ' ') {
+            revWord = str[i] + revWord
+        }
+        else {
+            revStr += revWord + ' '
+            revWord = ''
+        }
+    }
+    return revStr + revWord
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(reverseWords('The quick brown fox jumps over the lazy dog.'), 'ehT kciuq nworb xof spmuj revo eht yzal .god');
+    assert.strictEqual(reverseWords('apple'), 'elppa');
+    assert.strictEqual(reverseWords('a b c d'), 'a b c d');
+    assert.strictEqual(reverseWords('double  spaced  words'), 'elbuod  decaps  sdrow');
+  });
+});
